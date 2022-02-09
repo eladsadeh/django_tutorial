5925d0d6a955595d1d9a01f6659aebeca70ef4a7
eladsadeh <elad.sadeh1@.gmail.com>
2022-02-09 12:13:56 -0500

    priodic commit

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, Http404
+from django.template import ContextPopException
+from .models import Question
+from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You'r at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def all(request):
+    questions = Question.objects.all().values()
+    return JsonResponse(list(questions), safe=False)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
